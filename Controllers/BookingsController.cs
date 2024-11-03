52c8a501b47eb1050b6e2e905a34ff991a989843
Meghana-1609 <dogiparthimeghana1@gmail.com>
2024-11-02 22:25:43 -0500

    Added Controller Methods
    Controllers Consists of Http methods which are responsible of posting data to the  database.

---
+++ Controllers/BookingsController.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventManager.Models;
+
+namespace EventManager.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BookingsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bookings
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Bookings.Include(b => b.Client).Include(b => b.Event).Include(b => b.Venue);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Bookings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings
+                .Include(b => b.Client)
+                .Include(b => b.Event)
+                .Include(b => b.Venue)
+                .FirstOrDefaultAsync(m => m.BookingId == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // GET: Bookings/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId");
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId");
+            ViewData["VenueId"] = new SelectList(_context.Venues, "VenueId", "VenueId");
+            return View();
+        }
+
+        // POST: Bookings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClientId,EventId,VenueId,BookingDate")] BookingsViewModel bookingViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map BookingsViewModel to Booking entity
+                var booking = new Booking
+                {
+                    ClientId = bookingViewModel.ClientId,
+                    EventId = bookingViewModel.EventId,
+                    VenueId = bookingViewModel.VenueId,
+                    BookingDate = bookingViewModel.BookingDate
+                };
+
+                // Save the Booking entity to the database
+                _context.Add(booking);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Repopulate dropdown lists if ModelState is invalid
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId", bookingViewModel.ClientId);
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", bookingViewModel.EventId);
+            ViewData["VenueId"] = new SelectList(_context.Venues, "VenueId", "VenueId", bookingViewModel.VenueId);
+
+            return View(bookingViewModel);
+        }
+
+
+        // GET: Bookings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            // Map Booking entity to BookingEditViewModel
+            var bookingViewModel = new BookingEditViewModel
+            {
+                BookingId = booking.BookingId,
+                ClientId = booking.ClientId,
+                EventId = booking.EventId,
+                VenueId = booking.VenueId,
+                BookingDate = booking.BookingDate
+            };
+
+            // Populate dropdown lists
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId", bookingViewModel.ClientId);
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", bookingViewModel.EventId);
+            ViewData["VenueId"] = new SelectList(_context.Venues, "VenueId", "VenueId", bookingViewModel.VenueId);
+
+            return View(bookingViewModel);
+        }
+
+
+        // POST: Bookings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BookingId,ClientId,EventId,VenueId,BookingDate")] BookingEditViewModel bookingViewModel)
+        {
+            if (id != bookingViewModel.BookingId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Map BookingsViewModel to Booking entity
+                    var booking = await _context.Bookings.FindAsync(id);
+                    if (booking == null)
+                    {
+                        return NotFound();
+                    }
+
+                    booking.ClientId = bookingViewModel.ClientId;
+                    booking.EventId = bookingViewModel.EventId;
+                    booking.VenueId = bookingViewModel.VenueId;
+                    booking.BookingDate = bookingViewModel.BookingDate;
+
+                    _context.Update(booking);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookingExists(bookingViewModel.BookingId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Repopulate dropdown lists if ModelState is invalid
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId", bookingViewModel.ClientId);
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", bookingViewModel.EventId);
+            ViewData["VenueId"] = new SelectList(_context.Venues, "VenueId", "VenueId", bookingViewModel.VenueId);
+
+            return View(bookingViewModel);
+        }
+
+
+        // GET: Bookings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings
+                .Include(b => b.Client)
+                .Include(b => b.Event)
+                .Include(b => b.Venue)
+                .FirstOrDefaultAsync(m => m.BookingId == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // POST: Bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var booking = await _context.Bookings.FindAsync(id);
+            if (booking != null)
+            {
+                _context.Bookings.Remove(booking);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookingExists(int id)
+        {
+            return _context.Bookings.Any(e => e.BookingId == id);
+        }
+    }
+}
---
+++ Controllers/EventsController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventManager.Models;
+
+namespace EventManager.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EventsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Events
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Events.ToListAsync());
+        }
+
+        // GET: Events/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventType,Date,Budget")] EventEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    Console.WriteLine(error.ErrorMessage);  // Or use a logger
+                }
+                return View(model);
+            }
+
+            // Map EventEditViewModel to Event
+            var @event = new Event
+            {
+                EventType = model.EventType,
+                Date = model.Date,
+                Budget = model.Budget
+            };
+
+            // Save the Event entity to the database
+            _context.Add(@event);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // GET: Events/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            // Map Event to EventEditViewModel
+            var model = new EventEditViewModel
+            {
+                EventId = @event.EventId,
+                EventType = @event.EventType,
+                Date = @event.Date,
+                Budget = @event.Budget
+            };
+
+            return View(model);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, EventEditViewModel model)
+        {
+            if (id != model.EventId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Fetch the existing Event entity from the database
+                var @event = await _context.Events.FindAsync(id);
+                if (@event == null)
+                {
+                    return NotFound();
+                }
+
+                // Map changes from EventEditViewModel to Event entity
+                @event.EventType = model.EventType;
+                @event.Date = model.Date;
+                @event.Budget = model.Budget;
+
+                try
+                {
+                    // Save changes to the database
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(model.EventId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Return the view with the model if ModelState is invalid
+            return View(model);
+        }
+
+
+
+
+        // GET: Events/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @event = await _context.Events.FindAsync(id);
+            if (@event != null)
+            {
+                _context.Events.Remove(@event);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Events.Any(e => e.EventId == id);
+        }
+    }
+}
